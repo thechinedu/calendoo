4743e6ede474fe8c27efe459d04be59978229ba6
Chinedu Daniel <chinedudaniel7@gmail.com>
2023-07-26 08:45:16 +0200

    Fix failing build

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -3,7 +3,9 @@ import "@/styles/globals.css";
 import { initFirebase } from "@/utils/init-firebase";
 import type { AppProps } from "next/app";
 
-initFirebase();
+if (typeof window !== "undefined") {
+  initFirebase();
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
