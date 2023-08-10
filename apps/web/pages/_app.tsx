7c295ce2fdb16135f792e1396c655c99bca2baa0
Chinedu Daniel <chinedudaniel7@gmail.com>
2023-08-10 08:51:54 +0200

    Add signin page [status: complete]

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -3,11 +3,18 @@ import "@/styles/globals.css";
 
 import { initFirebase } from "@/utils/init-firebase";
 import type { AppProps } from "next/app";
+import { Noto_Sans } from "next/font/google";
+
+const noto = Noto_Sans({ subsets: ["latin"], weight: ["400", "700"] });
 
 if (typeof window !== "undefined") {
   initFirebase();
 }
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${noto.className} page-wrapper`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
